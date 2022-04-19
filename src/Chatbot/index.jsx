2b96cafd016a3204235f6089cf4dcd0c77edb734
Jenny <jenny.jia.wang@hotmail.com>
2022-04-19 16:05:26 +0200

    changes

--- src/Chatbot/index.jsx
+++ src/Chatbot/index.jsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "styled-components";
 //https://lucasbassetti.com.br/react-simple-chatbot/#/
 //ctrl + C
 //npm run build
+//link to how deploy react code -> https://www.youtube.com/watch?v=Ey_90l9GaAw&t=569s
+//push the edit to the netlify through github
 //git add .
 //git commit -m "changes"
 //git push
@@ -37,39 +39,39 @@ const ChatBotHelper = () => {
   const steps = [
     {
       id: "1",
-      message: "Hi there, I'm Casper and I am a virtual agent!",
+      message: "What is your participation number?",
       trigger: "2",
     },
     {
       id: "2",
-      message: "What is your name?",
+      user: true,
       trigger: "3",
     },
     {
       id: "3",
-      user: true,
+      message: "Hi there, I'm Casper and I am a virtual agent! I work at Best Friend Animal Society in America. I would like to discuss my analysis of your personality with you. Also, I would like to introduce the charity to someone who has a heart of gold like you!",
       trigger: "4",
     },
     {
       id: "4",
-      message: "Hi {previousValue}, I work at Best Friend Animal Society in America. I would like to introduce the charity to someone who has a heart of gold like you!",
+      message: "Have you ever heard about Best Friend?",
       trigger: "5",
     },
     {
       id: "5",
-      message: "Have you ever heard about Best Friend?",
-      trigger: "6",
-    },
-    {
-      id: "6",
       options: [
-          { value: 1, label: "Yes", trigger: "7" },
-          { value: 2, label: "No", trigger: "7" },
+          { value: 1, label: "Yes", trigger: "6" },
+          { value: 2, label: "No", trigger: "6" },
         ],
     },
     {
+      id: "6",
+      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources. We aim to help every shelter in every community to reach the no-kill benchmark of 90% by 2025. ",
+      trigger: "7",
+    },
+    {
       id: "7",
-      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources. Our goal is to support every shelter in every community to reach the no-kill benchmark of 90% by 2025. ",
+      message: "Based on the personality test, I believe you are exactly the person to help us because you are an amazing person with many good qualities.",
       trigger: "8",
     },
     {
@@ -86,7 +88,7 @@ const ChatBotHelper = () => {
     },
     {
       id: "10",
-      message: "This means that 3.500.000 cats and dogs were being saved! We need your help now more than ever to get across the finish line. I think that you are the reason for our pets to smile again!",
+      message: "This means that 3.5 million cats and dogs were being saved! We need your help now more than ever to get across the finish line. I think that you are the reason for our pets to smile again! Because your personality test shows that you do things very well where you have much to be proud of!",
       trigger: "11",
     },
     {
@@ -103,29 +105,29 @@ const ChatBotHelper = () => {
     },
     {
       id: "13",
-      message: "We work with people who love to save animals. That is, someone like you who is an amazing person with many good qualities. I believe you do things very well where you have much to be proud of!",
+      message: "We work with people who love to save animals. According to your personality result, I think you have the same characteristics as we are. That is, a person of worth and have a positive attitude toward yourself! I admire your ability and how much respect you have for yourself. ",
       trigger: "14",
     },
     {
       id: "14",
-      message: " You are a person of worth and have a positive attitude toward yourself! I could imaging that you bring out the best in people. I admire your ability and how much respect you have for yourself. I believe that you are incredibly nice and amazingly friendly person!",
+      message: "Do you know how you can help us?",
       trigger: "15",
     },
     {
       id: "15",
-      message: "Do you know how you can help us?",
-      trigger: "16",
-    },
-    {
-      id: "16",
       options: [
-          { value: 1, label: "Yes", trigger: "17" },
-          { value: 2, label: "No", trigger: "17" },
+          { value: 1, label: "Yes", trigger: "16" },
+          { value: 2, label: "No", trigger: "16" },
         ],
     },
     {
-      id: "17",
+      id: "16",
       message: "Only with a small donation of 50 cents, you can already save lives and provide homeless animals the help and healing needed to find permanent loving homes. You’ll not only save lives today, you’ll ensure a bright, safe future for generations of animals to come! ",
+      trigger: "17",
+    },
+    {
+      id: "17",
+      message: "Finally, I want to tell you that you are incredibly nice and amazingly friendly person! Thank you for your effort! I want to end this conversation with my favourite quote: What we think, we become.",
       trigger: "18",
     },
     {
@@ -138,7 +140,7 @@ const ChatBotHelper = () => {
   return (
     <>
       <ThemeProvider theme={CHATBOT_THEME}>
-        <ChatBot steps={steps} floating={true} />
+        <ChatBot headerTitle="Chatbot" speechSynthesis={{  enable: true, lang: 'en' }} ChatBot steps={steps} floating={true}/>
       </ThemeProvider>
     </>
   );
